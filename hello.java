194800300907224ec228b490f86a35c6fd6bee70
abhi-tech21 <vvce23cse0295@vvce.ac.in>
2025-12-04 18:13:31 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
-public class hello
-{
-  public static void main(String[] args){
-    System.out.println("Devops lab");
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins and GitHub Integration!");
+    }
 }
